4eae19b46a673cfaab10a05fd35f3a7d41457f51
Jey <jey.nandakumar@gmail.com>
2018-04-12 15:26:01 +0100

    ++webhook post handler.

--- web.rb
+++ web.rb
@@ -1,12 +1,21 @@
 require 'sinatra'
 require 'json'
 
+# SECRET_TOKEN = ''
+
 get '/' do
-  'Auto WCAG Deployer'
+  'Auto WCAG Deployer: Which listens to GitHub Webhook to rebuild gh-pages.'
 end
 
-post '/api' do
+post '/deploy' do
   request.body.rewind
-  data = JSON.parse request.body.read
-  "Hello #{data}!"
+  payload_body = request.body.read
+  # verify_signature(payload_body)
+  push = JSON.parse(params[:payload])
+  "Payload: #{push.inspect}!"
 end
+
+# def verify_signature(payload_body)
+#   signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+#   return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+# end
